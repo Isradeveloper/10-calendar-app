64107883cd12de5222d6c34fcba93cf9d48d7af3
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-27 07:49:38 -0500

    FrontEnd Finalizado

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -1,14 +1,15 @@
-import { useCalendarStore } from '../../hooks'
+import { useCalendarStore, useUiStore } from '../../hooks'
 
 export const FabDelete = () => {
   const { startDeletingEvent, hasEventSelected } = useCalendarStore()
+  const { isDateModalOpen } = useUiStore()
 
   const handleDelete = () => {
     startDeletingEvent()
   }
 
   return (
-    <button className='btn btn-danger fab-danger' onClick={(e) => { handleDelete() }} style={{ display: hasEventSelected ? '' : 'none' }}>
+    <button className='btn btn-danger fab-danger' onClick={(e) => { handleDelete() }} style={{ display: (hasEventSelected && isDateModalOpen) ? '' : 'none' }}>
       <i className='fas fa-trash-alt' />
     </button>
   )
