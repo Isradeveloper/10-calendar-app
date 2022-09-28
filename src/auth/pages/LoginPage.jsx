8a67085070108363f382f1374c0d2cc77643bd55
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-28 14:11:22 -0500

    Auth State y Formularios de auth

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,38 +1,64 @@
-import React from 'react';
-import './LoginPage.css';
+import React from 'react'
+import { useForm } from '../../hooks'
+import './LoginPage.css'
 
-export function LoginPage() {
+const loginFormFields = {
+  loginEmail: '',
+  loginPassword: ''
+}
+
+const registerFormFields = {
+  registerName: '',
+  registerEmail: '',
+  registerPassword: '',
+  registerPassword2: ''
+}
+
+export function LoginPage () {
+  const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields)
+  const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm(registerFormFields)
+
+  const loginSubmit = (event) => {
+    event.preventDefault()
+    console.log({ loginEmail, loginPassword })
+  }
+
+  const registerSubmit = (event) => {
+    event.preventDefault()
+    console.log({ registerName, registerEmail, registerPassword, registerPassword2 })
+  }
+
   return (
-    <div className="container-fluid login-container">
+    <div className='container-fluid login-container'>
 
-      <div className="row d-flex">
+      <div className='row d-flex'>
 
-        <div className="col-md-6 login">
+        <div className='col-md-6 login'>
           <h3>Ingreso</h3>
 
-          <form>
-            <div className="mt-5">
-              <input type="email" className="form-control mb-3" id="email-login" placeholder="name@example.com" />
-              <input type="password" className="form-control mb-3" id="password-login" placeholder="Ingrese contraseña" />
-              <button type="submit" className="btn-submit login">Login</button>
+          <form onSubmit={loginSubmit}>
+            <div className='mt-5'>
+              <input type='email' name='loginEmail' value={loginEmail} onChange={onLoginInputChange} className='form-control mb-3' id='email-login' placeholder='name@example.com' />
+              <input type='password' name='loginPassword' value={loginPassword} onChange={onLoginInputChange} className='form-control mb-3' id='password-login' placeholder='Ingrese contraseña' />
+              <button type='submit' className='btn-submit login'>Login</button>
             </div>
           </form>
 
         </div>
 
-        <div className="col-md-6 register">
+        <div className='col-md-6 register'>
           <h3>Registro</h3>
-          <form>
-            <div className="mt-5">
-              <input type="text" className="form-control mb-3" id="name-register" placeholder="Ingrese nombre" />
-              <input type="email" className="form-control mb-3" id="email-register" placeholder="name@example.com" />
-              <input type="password" className="form-control mb-3" id="password-register" placeholder="Ingrese contraseña" />
-              <input type="password" className="form-control mb-3" id="password-repeat-register" placeholder="Repita la contraseña" />
-              <button type="submit" className="btn-submit register">Crear cuenta</button>
+          <form onSubmit={registerSubmit}>
+            <div className='mt-5'>
+              <input type='text' name='registerName' value={registerName} onChange={onRegisterInputChange} className='form-control mb-3' id='name-register' placeholder='Ingrese nombre' />
+              <input type='email' name='registerEmail' value={registerEmail} onChange={onRegisterInputChange} className='form-control mb-3' id='email-register' placeholder='name@example.com' />
+              <input type='password' name='registerPassword' value={registerPassword} onChange={onRegisterInputChange} className='form-control mb-3' id='password-register' placeholder='Ingrese contraseña' />
+              <input type='password' name='registerPassword2' value={registerPassword2} onChange={onRegisterInputChange} className='form-control mb-3' id='password-repeat-register' placeholder='Repita la contraseña' />
+              <button type='submit' className='btn-submit register'>Crear cuenta</button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../auth';
-import { CalendarPage } from '../calendar/pages/CalendarPage';
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { CalendarPage } from '../calendar/pages/CalendarPage'
 
-export function AppRouter() {
-
-  const authStatus = 'authenticated'; // not-authenticated;
+export function AppRouter () {
+  const authStatus = 'not-authenticated' // 'authenticated' // not-authenticated;
 
   return (
     <Routes>
       {
         (authStatus === 'not-authenticated')
         // Si no está autenticado e ingresa a auth - se genera el login
-          ? <Route path="auth/*" element={<LoginPage />} />
+          ? <Route path='auth/*' element={<LoginPage />} />
         // Si está autenticado e ingresa otro path - se genera calendar
-          : <Route path="/*" element={<CalendarPage />} />
+          : <Route path='/*' element={<CalendarPage />} />
       }
 
       {/* Si no está autenticado  */}
-      <Route path="/*" element={<Navigate to="auth/" />} />
+      <Route path='/*' element={<Navigate to='auth/' />} />
     </Routes>
-  );
+  )
 }
